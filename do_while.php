63ee6a38e33dc16dd271b46de52b678a17eed544
Andrew Powell-Morse <andrewpomo815@gmail.com>
2016-04-15 13:22:16 -0500

    Finished second step (Count backwards by fives from 100 to -10.)

--- do_while.php
+++ do_while.php
@@ -2,7 +2,18 @@
 
 $a = 0;
 
+echo "Count by twos to 100!\n";
+
 do {
 	echo $a."\n";
 	$a += 2;
 } while ($a < 102);
+
+echo "\nCount by -fives to -10!\n";
+
+$b = 100;
+
+do {
+	echo $b."\n";
+	$b -= 5;
+} while ($b > -15);
