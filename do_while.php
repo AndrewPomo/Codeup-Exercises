2535c542a8a4df6357c9590ed6b456e69477f105
Andrew Powell-Morse <andrewpomo815@gmail.com>
2016-04-15 13:24:38 -0500

    Finished third portion of exercise (Starting with 2, square the result, but have no results greater than 1,000,000). Finished exercise.

--- do_while.php
+++ do_while.php
@@ -17,3 +17,12 @@ do {
 	echo $b."\n";
 	$b -= 5;
 } while ($b > -15);
+
+echo "\nStarting with 2, square the result, but have no results greater than 1,000,000!\n";
+
+$c = 2;
+
+do {
+	echo $c."\n";
+	$c = $c*$c;
+} while ($c < 100000000);
