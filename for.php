d93a6b33f35862717bf1c900595ec5117dd6ed06
Andrew Powell-Morse <andrewpomo815@gmail.com>
2016-04-18 12:09:06 -0500

    added increment selector. default is 1

--- for.php
+++ for.php
@@ -6,6 +6,16 @@ $startNum = fgets(STDIN);
 fwrite(STDOUT, "\nAnd where would you like me to stop?\n");
 $endNum = fgets(STDIN);
 
-for ($i=$startnum; $i < $endNum; $i++) { 
+fwrite(STDOUT, "\nAnd finally, what would you like me to count by? (To count by ones enter '1', to count by fives enter '5'.)\n");
+$increment = fgets(STDIN);
+echo $increment;
+
+if (ctype_digit($increment)) {
+	return;
+} else {
+	$increment = 1;
+}
+
+for ($i=$startNum; $i < $endNum+1; $i+=$increment) { 
 	echo "$i\n";
 }
