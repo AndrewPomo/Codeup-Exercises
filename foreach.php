f40e066c32e5e6d8a1642eac9e9caf9dde1fa0e8
Andrew Powell-Morse <andrewpomo815@gmail.com>
2016-04-18 15:01:59 -0500

    finished first portion of assignment

--- foreach.php
+++ foreach.php
@@ -8,5 +8,34 @@ $things = [
 	3.14, 
 	"12 + 7", 
 	false, 
-	(string) 11];
+	(string) 11
 ];
+
+$thingNum = 1;
+
+foreach ($things as $thingValue) {
+	if (is_array($thingValue)) {
+		echo "Thing number $thingNum is an array.\n";
+		$thingNum++;
+	} elseif (is_bool($thingValue)) {
+		echo "Thing number $thingNum is a boolean.\n";
+		$thingNum++;
+	}elseif (is_float($thingValue)) {
+		echo "Thing number $thingNum is a floating point number.\n";
+		$thingNum++;
+	}elseif (is_int($thingValue)) {
+		echo "$thingValue is an integer.\n";
+		$thingNum++;
+	}elseif (is_null($thingValue)) {
+		echo "Thing number $thingNum is nothing.\n";
+		$thingNum++;
+	}elseif (is_numeric($thingValue)) {
+		echo "Thing number $thingNum is a numeric value.\n";
+		$thingNum++;
+	}
+	// }elseif (is_scalar($thingValue)) {
+	// 	echo "$thingValue is a scalar value."
+	// }elseif (is_string($thingValue)) {
+	// 	echo "$thingValue is a string."
+	// }
+};
