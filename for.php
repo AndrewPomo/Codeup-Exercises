afa9f06064ad4ca596eb0d191413558fd37f3b94
Andrew Powell-Morse <andrewpomo815@gmail.com>
2016-04-18 13:28:39 -0500

    Finished. Nothin will happen if an input of the wrong type is given.

--- for.php
+++ for.php
@@ -1,17 +1,27 @@
 <?php 
 
-fwrite(STDOUT, "\nHiya! I am a silly counting computer. You tell me how to count, and I'll count it for you! What number would you like me to start with?\n");
-$startNum = fgets(STDIN);
+do {
+	fwrite(STDOUT, "\nHiya! Tell me how to count, and I'll do it! What number would you like me to start with?\n");
+	$startNum = trim(fgets(STDIN));
+	echo $startNum;
+} while (!ctype_digit($startNum));
+echo $startNum;
 
+do {
 fwrite(STDOUT, "\nAnd where would you like me to stop?\n");
-$endNum = fgets(STDIN);
+$endNum = trim(fgets(STDIN));
+} while (ctype_digit($endNum) == false);
+echo $endNum;
 
+do {
 fwrite(STDOUT, "\nAnd finally, what would you like me to count by? (To count by ones enter '1', to count by fives enter '5'.)\n");
-$increment = fgets(STDIN);
+$increment = trim(fgets(STDIN));
 echo $increment;
 
+} while (ctype_digit($increment) == false);
+
 if (ctype_digit($increment)) {
-	return;
+	$increment = $increment;
 } else {
 	$increment = 1;
 }
