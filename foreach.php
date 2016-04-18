3ccefd07dc1d8a6ee8023598dcc55adc3b6ea344
Andrew Powell-Morse <andrewpomo815@gmail.com>
2016-04-18 15:13:19 -0500

    Added second portion of exercise

--- foreach.php
+++ foreach.php
@@ -13,6 +13,8 @@ $things = [
 
 $thingNum = 1;
 
+echo "\n First loop \n";
+
 foreach ($things as $thingValue) {
 	if (is_array($thingValue)) {
 		echo "Thing number $thingNum is an array.\n";
@@ -31,6 +33,38 @@ foreach ($things as $thingValue) {
 		$thingNum++;
 	}elseif (is_numeric($thingValue)) {
 		echo "Thing number $thingNum is a numeric value.\n";
+		$thingNum++;
+	}elseif (is_string($thingValue)) {
+		echo "Thing number $thingNum is a string.\n";
+	}
+	
+	// }elseif (is_scalar($thingValue)) {
+	// 	echo "$thingValue is a scalar value."
+};
+
+echo "\n Second loop \n";
+
+$thingNum = 1;
+foreach ($things as $thingValue) {
+	if (is_scalar($thingValue)) {
+		if (is_bool($thingValue)) {
+			echo "Thing number $thingNum is a boolean.\n";
+			$thingNum++;
+		}elseif (is_float($thingValue)) {
+			echo "Thing number $thingNum is a floating point number.\n";
+			$thingNum++;
+		}elseif (is_int($thingValue)) {
+			echo "$thingNum is an integer.\n";
+			$thingNum++;
+		}elseif (is_null($thingValue)) {
+			echo "Thing number $thingNum is nothing.\n";
+			$thingNum++;
+		}elseif (is_string($thingValue)) {
+			echo "Thing number $thingNum is a string.\n";
+			$thingNum++;
+		}
+	} else {
+		echo "$thingNum is not scalar, so it doesn't matter.\n";
 		$thingNum++;
 	}
 	// }elseif (is_scalar($thingValue)) {
