29a04d499a6c0db9630b9d813e2e3e23c2004d9d
Andrew Powell-Morse <andrewpomo815@gmail.com>
2016-04-18 09:24:40 -0500

    Got grammar stuff right.

--- bottles.php
+++ bottles.php
@@ -6,15 +6,18 @@ do {
 	do { 
 		echo "$bottleNum bottles of beer on the wall, $bottleNum bottles of beer.\n";
 		$bottleNum--;
-		echo "Take one down and pass it around, $bottleNum bottles of beer on the wall.\n\n";
+		echo "Take one down, pass it around, $bottleNum bottles of beer on the wall.\n\n";
 		
-	} while ($bottleNum >1);
+	} while ($bottleNum >2);
+		echo "$bottleNum bottles of beer on the wall, $bottleNum bottles of beer.\n";
+		$bottleNum--;
+		echo "Take one down, pass it around, $bottleNum bottle of beer on the wall.\n\n";
 
-	echo "$bottleNum bottle of beer on the wall, $bottleNum bottle of beer.\n";
-	$bottleNum--;
-	echo "Take one down and pass it around, no more bottles of beer on the wall.\n\n";
+		echo "$bottleNum bottle of beer on the wall, $bottleNum bottle of beer.\n";
+		$bottleNum--;
+		echo "Take one down, pass it around, no more bottles of beer on the wall.\n\n";
 
-	echo "No more bottles of beer on the wall, no more bottles of beer.\n";
-	$bottleNum += 99;
-	echo "Go to the store and buy some more, $bottleNum bottles of beer on the wall.\n\n";
+		echo "No more bottles of beer on the wall, no more bottles of beer.\n";
+		$bottleNum += 99;
+		echo "Go to the store, buy some more, $bottleNum bottles of beer on the wall.\n\n";
 } while ($bottleNum >= 0);
