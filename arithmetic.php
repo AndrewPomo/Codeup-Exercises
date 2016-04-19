607b1f3116fd8c6b4ed2e058511fede41ab63a80
Andrew Powell-Morse <andrewpomo815@gmail.com>
2016-04-19 14:34:24 -0500

    finished whole exercise.

--- arithmetic.php
+++ arithmetic.php
@@ -1,5 +1,8 @@
 <?php
 
+$a = 50;
+$b = 5;
+
 function add($a, $b)
 {
 	return $a + $b;
@@ -26,19 +29,19 @@ function modulus($a, $b)
 }
 
 echo "Add 50 & 5\n";
-echo add(50, 5);
+echo add($a, $b);
 echo PHP_EOL;
 echo "Subtract 5 from 50\n";
-echo subtract(50, 5);
+echo subtract($a, $b);
 echo PHP_EOL;
 echo "Multiply 5 by 50\n";
-echo multiply(50, 5);
+echo multiply($a, $b);
 echo PHP_EOL;
 echo "Divide 50 by 5\n";
-echo divide(50, 5);
+echo divide($a, $b);
 echo PHP_EOL;
 echo "Find remainder of 50 divided by 5\n";
-echo modulus(50, 5);
+echo modulus($a, $b);
 echo PHP_EOL;
 
 
