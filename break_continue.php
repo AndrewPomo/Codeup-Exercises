831abb817ba74c6226ed7d89c5415f78e5b9afd0
Andrew Powell-Morse <andrewpomo815@gmail.com>
2016-04-19 11:26:15 -0500

    Finished

--- break_continue.php
+++ break_continue.php
@@ -1,9 +1,18 @@
 <?php
-
+echo "first loop\n";
 for ($i=0; $i <= 100; $i++) { 
 	if ($i % 2 != 0){
 		continue;
 	} else {
 		echo "$i\n";
+	}
+}
+
+echo "next loop\n";
+
+for ($i=0; $i <= 100; $i++) { 
+	echo "$i\n";
+	if ($i == 10){
+		break;
 	}
 }
