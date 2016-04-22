f5fc740203ba7667fb55b39a2a4f83ea602fad75
Andrew Powell-Morse <andrewpomo815@gmail.com>
2016-04-22 11:11:22 -0500

    This works.

--- contact-parser.php
+++ contact-parser.php
@@ -1,12 +1,27 @@
 <?php
+function phoneDash($number)
+{
+	return substr($number, 0, 3) .'-'.substr($number, 3, 3) .'-'.substr($number, 6);
+}
 
 function parseContacts($filename)
 {
     $contacts = array();
-
+    $newContacts = array();
     // todo - read file and parse contacts
-
-    return $contacts;
+    $handle = fopen($filename, 'r');
+	$contactFile = fread($handle, filesize($filename));
+	$contacts = explode("\n", trim($contactFile));
+	foreach ($contacts as $contact => $contactInfo) {
+		$contactInfo = explode("|", $contactInfo);
+		$contactInfo = [
+			"name" => $contactInfo[0],
+			"number" => phoneDash($contactInfo[1]),
+		];
+		array_push($newContacts, $contactInfo);
+	}
+    fclose($handle);
+    return $newContacts;
 }
 
-var_dump(parseContacts('contacts.txt'));
+print_r(parseContacts('contacts.txt'));
