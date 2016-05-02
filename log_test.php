620addb708fbe849cedac7ed85a4f88c23c66296
Andrew Powell-Morse <andrewpomo815@gmail.com>
2016-05-02 12:00:23 -0500

    Cleaned this up a bit.

--- log_test.php
+++ log_test.php
@@ -5,9 +5,6 @@ $newLog = new Log();
 $newLog->currentTime = date('H:i:s');
 $newLog->currentDate = date('Y-m-d');
 $newLog->filename = "log-$newLog->currentDate.log";
+$newLog->info("If I slip, then I'm slippin'.");
+$newLog->error("I'm slippin'.");
 
-$newLog->logMessage("INFO", "this works.");
-// $newLog->info("If I slip, then I'm slippin'.");
-// $newLog->error("I'm slippin'.");
-
- ?>
